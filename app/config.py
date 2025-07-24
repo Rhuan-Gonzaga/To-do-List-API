bc4bed0442ee4c598a66cf757d868fedc8769cfd
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2025-07-24 07:00:50 -0300

    add: rota de login com geração de token jwt

--- app/config.py
+++ app/config.py
@@ -11,3 +11,4 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "minhachavesecreta")
--- app/views/users.py
+++ app/views/users.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
+from datetime import datetime, timedelta
 from app.schemas import UserSchema
+import jwt
 from app import db
 
 users_bp = Blueprint('users', __name__)
@@ -11,7 +13,7 @@ users_schema = UserSchema(many=True)
 
 @users_bp.route('/signup', methods=['POST'])
 def create_user():
-    data = request.json
+    data = request.get_json()
 
     user = User(
         username = data['username'],
@@ -22,7 +24,30 @@ def create_user():
     
     return jsonify({"message": "Usuário cadastrado com sucesso!"})
     
+@users_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
 
+    #Verifica se o campo da request é vazio
+    if not data or not data.get('username') or not data.get('password'):
+        return jsonify({'message': 'Usuário e senha são obrigatórios'}), 400
+    
+    #retorna o usuario
+    user = User.query.filter_by(username=data['username']).first()
+
+    #verificando se a senha do usuario é igual a do banco
+    if not user or not check_password_hash(user.password_hash, data['password']):
+        return jsonify({'message': 'Usuário ou senha inválidos'}), 401
+        
+    #gerar token
+    payload ={
+        'user_id': user.id,
+        'exp': datetime.utcnow() + timedelta(hours=1) 
+    }
+
+    token = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
+
+    return jsonify({'token': token, 'username': user.username})
 
 
 
